6024072fb6d4a90aa883a35458302ab71ad474ba
Sudhir128 <140303452+Sudhir128@users.noreply.github.com>
2024-05-15 14:14:02 +0530

    Delete MyApplication2 directory

--- MyApplication2/app/src/main/java/com/example/myapplication/Dealer_model.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-import com.google.firebase.firestore.PropertyName
-
-class Dealer_model {
-    @get:PropertyName("name") @set:PropertyName("name") var name: String = "",
-    @get:PropertyName("location") @set:PropertyName("location") var location: String = "",
-    @get:PropertyName("contact") @set:PropertyName("contact") var contact: String = ""
-}
--- MyApplication2/app/src/main/java/com/example/myapplication/car_model.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-data class Car(
-    val name: String,
-    val imageUrl: String,
-    val year: String,
-    val kms: String
-)
-
